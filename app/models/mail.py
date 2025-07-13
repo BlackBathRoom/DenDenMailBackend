b45fed116024638cdf7a24f52ee3b3deb0cde0a8
haruki26 <167726701+haruki26@users.noreply.github.com>
2025-07-14 01:36:18 +0900

    add: メールのモデルを定義

---
+++ app/models/mail.py
@@ -0,0 +1,95 @@
+from datetime import datetime
+
+from app_conf import MailVender
+from models.common import BaseSQLModel
+from sqlmodel import Field, SQLModel
+
+
+class BaseMail(SQLModel):
+    """メールのベースモデル.
+
+    Attributes:
+        message_id (str): メッセージID.
+        subject (str): 件名.
+        received_at (datetime): 受信日時.
+        sender_name (str): 送信者.
+        sender_address (str): 送信者アドレス.
+        mail_folder (str): フォルダ.
+        is_read (bool): 既読フラグ.
+        vender (MailVender): メールクライアント名.
+    """
+
+    message_id: str = Field(index=True, unique=True)
+    subject: str
+    received_at: datetime
+    sender_name: str
+    sender_address: str
+    mail_folder: str
+    is_read: bool = False
+    vender: MailVender
+
+
+class Mail(BaseMail, BaseSQLModel, table=True):
+    """メールモデル.
+
+    Attributes:
+        id (int): アプリ内メールID.
+        message_id (str): メッセージID.
+        subject (str): 件名.
+        received_at (datetime): 受信日時.
+        sender_name (str): 送信者.
+        sender_address (str): 送信者アドレス.
+        mail_folder (str): フォルダ.
+        is_read (bool): 既読フラグ.
+        created_at (datetime): アプリdbに登録された日時.
+        updated_at (datetime): アプリdbで更新された日時.
+        vender (MailVender): メールクライアント名.
+    """
+
+
+class MailCreate(BaseMail):
+    """メール作成用モデル.
+
+    アプリ内にメールを新規登録する際に使用されるモデル。
+
+    Attributes:
+        message_id (str): メッセージID.
+        subject (str): 件名.
+        received_at (datetime): 受信日時.
+        sender_name (str): 送信者.
+        sender_address (str): 送信者アドレス.
+        mail_folder (str): フォルダ.
+        is_read (bool): 既読フラグ.
+        vender (MailVender): メールクライアント名.
+    """
+
+
+class MailRead(BaseMail):
+    """メール読み取り用モデル.
+
+    メールを読み取る際に使用されるモデル。
+
+    Attributes:
+        id (int): アプリ内メールID.
+        message_id (str): メッセージID.
+        subject (str): 件名.
+        received_at (datetime): 受信日時.
+        sender_name (str): 送信者.
+        sender_address (str): 送信者アドレス.
+        mail_folder (str): フォルダ.
+        is_read (bool): 既読フラグ.
+        created_at (datetime): アプリdbに登録された日時.
+        vender (MailVender): メールクライアント名.
+    """
+
+
+class MailUpdate(SQLModel):
+    """メール更新用モデル.
+
+    メールを更新する際に使用されるモデル。
+
+    Attributes:
+        is_read (bool): 既読フラグ.
+    """
+
+    is_read: bool | None = None
