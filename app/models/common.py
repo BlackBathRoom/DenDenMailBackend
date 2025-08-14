4f87ccaff8357c83e4837620511c44a405af3ebe
haruki26 <167726701+haruki26@users.noreply.github.com>
2025-08-15 03:13:07 +0900

    update: タイムスタンプとIDでモデルを分割
    複合主キーのテーブルに対応のため

--- app/models/common.py
+++ app/models/common.py
@@ -3,18 +3,21 @@ from datetime import datetime
 from sqlmodel import Field, SQLModel
 
 
-class BaseSQLModel(SQLModel):
-    """ベースSQLモデル.
+class TimestampedSQLModel(SQLModel):
+    """タイムスタンプ共通モデル."""
 
-    Attributes:
-        id (int): アプリ内ID.
-        created_at (datetime): 作成日時.
-        updated_at (datetime): 更新日時.
-    """
-
-    id: int | None = Field(primary_key=True)
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime = Field(
         default_factory=datetime.now,
         sa_column_kwargs={"onupdate": datetime.now},
     )
+
+
+class IdModel(SQLModel):
+    """数値ID主キーモデル."""
+
+    id: int | None = Field(primary_key=True)
+
+
+class BaseSQLModel(IdModel, TimestampedSQLModel):
+    """従来のベースSQLモデル(id + タイムスタンプ)."""
