8cae544fa8eb8569645827a01bc52eba463f080b
InoueToranosuke <167955585+InoueToranosuke@users.noreply.github.com>
2025-08-23 23:36:54 +0900

    main.pyのimport修正

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from routers.demo import router as demo_router
+from routers.demo_mail import router as demo_mail_router
+from routers.demo_rules import router as demo_rules_router
+from routers.demo_summary import router as demo_summary_router
 from starlette.middleware.cors import CORSMiddleware
 
 
@@ -15,7 +17,9 @@ def setup_server() -> FastAPI:
     )
 
     routers = [
-        ("api", demo_router),
+        ("api", demo_summary_router),
+        ("api", demo_mail_router),
+        ("api", demo_rules_router),
     ]
 
     for prefix, router in routers:
