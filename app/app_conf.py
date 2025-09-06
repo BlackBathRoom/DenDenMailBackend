324565fbccd27e5eac94800c8b6e3001a3bb6808
haruki26 <167726701+haruki26@users.noreply.github.com>
2025-09-07 02:15:39 +0900

    chore: openvinoの設定をアプリ内で統一

--- app/app_conf.py
+++ app/app_conf.py
@@ -21,3 +21,6 @@ AI_MODEL_PATH = BASE_DIR / "ai_models"
 # database
 DB_PATH = BASE_DIR / "database" / "db.sqlite3"
 engine = create_engine(f"sqlite:///{DB_PATH}", echo=True, connect_args={"check_same_thread": False})
+
+# OpenVINO
+OV_CONFIG = {"PERFORMANCE_HINT": "LATENCY", "NUM_STREAMS": "1", "CACHE_DIR": ""}
