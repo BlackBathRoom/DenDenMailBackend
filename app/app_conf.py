af70f46e9a5be12c0496adf1b87b5bc2cb09c184
haruki26 <167726701+haruki26@users.noreply.github.com>
2025-09-13 01:03:07 +0900

    refactor: エンジン取得関数をサービス配下へ

--- app/app_conf.py
+++ app/app_conf.py
@@ -1,8 +1,6 @@
 from enum import Enum
 from pathlib import Path
 
-from sqlmodel import create_engine
-
 
 # type
 class MailVender(str, Enum):
@@ -14,13 +12,13 @@ class MailVender(str, Enum):
 
 # path
 BASE_DIR = Path(__file__).parent.parent
+BASE_DIR.absolute()
 
 # ai model
 AI_MODEL_PATH = BASE_DIR / "ai_models"
 
 # database
 DB_PATH = BASE_DIR / "database" / "db.sqlite3"
-engine = create_engine(f"sqlite:///{DB_PATH}", echo=True, connect_args={"check_same_thread": False})
 
 # OpenVINO
 OV_CONFIG = {"PERFORMANCE_HINT": "LATENCY", "NUM_STREAMS": "1", "CACHE_DIR": ""}
