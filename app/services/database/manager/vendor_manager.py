ee70e3d0b5b5d7eeb4350fe74c8baecbd8ca3763
haruki26 <167726701+haruki26@users.noreply.github.com>
2025-09-13 14:13:15 +0900

    add: ベンダーが登録済みかを確認するメソッド、登録するメソッドを作成

--- app/services/database/manager/vendor_manager.py
+++ app/services/database/manager/vendor_manager.py
@@ -1,5 +1,12 @@
+from sqlalchemy import Engine
+
+from app_conf import MailVendor
 from models.vendor import Vendor, VendorCreate
 from services.database.manager.base import BaseDBManager
+from services.database.manager.condition import FieldCondition
+from utils.logging import get_logger
+
+logger = get_logger(__name__)
 
 
 class VendorDBManager(BaseDBManager[Vendor, VendorCreate, None]):
@@ -7,3 +14,30 @@ class VendorDBManager(BaseDBManager[Vendor, VendorCreate, None]):
 
     def __init__(self, model: type[Vendor] = Vendor) -> None:
         super().__init__(model)
+
+    def is_registered(self, engine: Engine, vendor: MailVendor) -> bool:
+        """指定されたベンダーが登録されているか確認する.
+
+        Args:
+            engine (Engine): SQLAlchemyエンジン.
+            vendor (MailVendor): 確認するベンダー.
+
+        Returns:
+            bool: 登録されていれば True、そうでなければ False.
+        """
+        stmt = self.read(engine, conditions=[FieldCondition(operator="eq", field="name", value=vendor.value)])
+        return stmt is not None
+
+    def register(self, engine: Engine, vendor: MailVendor) -> None:
+        """指定されたベンダーを登録する.
+
+        Args:
+            engine (Engine): SQLAlchemyエンジン.
+            vendor (MailVendor): 登録するベンダー.
+        """
+        if self.is_registered(engine, vendor):
+            logger.info("Vendor %s is already registered.", vendor.value)
+            return
+        create_obj = VendorCreate(name=vendor.value)
+        self.create(engine, create_obj)
+        logger.info("Vendor %s registered successfully.", vendor.value)
