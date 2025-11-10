673c7d44f6e328082f903f6124819a217dab699d
Itsuki256 <ktc24a31e0016@edu.kyoto-tech.ac.jp>
2025-11-10 16:15:25 +0900

    機能追加: search_routerをエクスポートに追加
    ベクトル検索ルーターをルーターモジュールに登録

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,5 +1,6 @@
 from .messages import router as messages_router
 from .rules import router as rules_router
+from .search import router as search_router
 from .summary import router as summary_router
 
-__all__ = ["messages_router", "rules_router", "summary_router"]
+__all__ = ["messages_router", "rules_router", "search_router", "summary_router"]
